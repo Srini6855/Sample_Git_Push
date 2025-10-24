ad331fa0ee7e0abf95e5f706408c20c4242ec3eb
srin-omr <srinivasanomrbranch@gmail.com>
2025-10-24 16:36:58 +0530

    Oct 24

---
+++ src/test/java/org/sample/Sampleee.java
@@ -0,0 +1,17 @@
+package org.sample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Sampleee {
+	public static void main(String[] args) {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--headless");
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		driver.get("https://www.omrbranch.com/");
+		System.out.println(driver.findElement(By.cssSelector(".btn.btn-training")).getText());
+	}
+
+}
